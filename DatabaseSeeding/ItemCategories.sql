71edc4ca67cfd6da06bddb587c5c9203ca61b184
ambid17 <ambid17@gmail.com>
2025-10-16 18:58:41 -0500

    seed db

--- DatabaseSeeding/ItemCategories.sql
+++ DatabaseSeeding/ItemCategories.sql
@@ -1,12 +1,13 @@
-﻿delete from "ItemCategories";
+﻿truncate "ItemCategories" CASCADE; -- Clears the table and any dependent data
+ALTER SEQUENCE "ItemCategories_Id_seq" RESTART WITH 1; -- Resets the primary key sequence
 
 insert into "ItemCategories"("Name")
 values
-('Favorites'),
 ('Equipment'),
 ('Weapon Accessories'),
 ('Weapon Bolts'),
 ('Ammo'),
+('Medical Supplies'),
 ('Tactical Items'),
 ('Keys'),
 ('Miscellaneous'),
